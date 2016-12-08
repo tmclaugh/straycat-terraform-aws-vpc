71d5a1fe9b1155997527be43d1bb6da1110654c5
Tom McLaughlin <tom.mclaughlin@threatstack.com>
2016-12-07 20:38:02 -0500

    Setup AWS peering

--- environments/master/main.tf
+++ environments/master/main.tf
@@ -38,3 +38,77 @@ module "vpc_private" {
   vpc_name = "private"
 }
 
+resource "aws_vpc_peering_connection" "private_public" {
+  peer_owner_id = "${var.aws_account_id}"
+  peer_vpc_id = "${module.vpc_public.vpc_id}"
+  vpc_id = "${module.vpc_private.vpc_id}"
+  auto_accept = true
+
+  accepter {
+    allow_remote_vpc_dns_resolution = true
+  }
+
+  # must be false because we do not to DNS hostnames in the vpc_private
+  requester {
+    allow_remote_vpc_dns_resolution = false
+  }
+
+  tags {
+    Name = "private <-> public"
+    terraform = "true"
+  }
+}
+
+# FIXME: Need better way to get count. Can't set to length of a module's
+# output.  Couldn't depend on an output too for some reason.
+resource "aws_route" "vpc_peer_private" {
+  count                     = "${length(var.aws_availability_zones)}"
+  route_table_id            = "${element(module.vpc_private.route_table_ids_private, count.index)}"
+  destination_cidr_block    = "${module.vpc_public.vpc_cidr_block}"
+  vpc_peering_connection_id = "${aws_vpc_peering_connection.private_public.id}"
+}
+
+
+resource "aws_route" "vpc_peer_public" {
+  count                     = "${length(var.aws_availability_zones)}"
+  route_table_id            = "${element(module.vpc_public.route_table_ids_public, count.index)}"
+  destination_cidr_block    = "${module.vpc_private.vpc_cidr_block}"
+  vpc_peering_connection_id = "${aws_vpc_peering_connection.private_public.id}"
+}
+
+resource "aws_security_group" "public_bastion_ssh" {
+  name        = "${module.vpc_public.vpc_name}-bastion-ssh"
+  description = "public-bastion: allow SSH"
+  vpc_id = "${module.vpc_public.vpc_id}"
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name      = "${module.vpc_public.vpc_name}-bastion-ssh"
+    terraform = "true"
+  }
+}
+
+resource "aws_security_group" "private_bastion_ssh_traffic" {
+  name        = "${module.vpc_private.vpc_name}-bastion-ssh-traffic"
+  description = "private-bastion-traffic: allow SSH from bastion"
+  vpc_id = "${module.vpc_private.vpc_id}"
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    security_groups = ["${aws_security_group.public_bastion_ssh.id}"]
+  }
+
+  tags = {
+    Name      = "${module.vpc_private.vpc_name}-bastion-ssh-traffic"
+    terraform = "true"
+  }
+}
+
