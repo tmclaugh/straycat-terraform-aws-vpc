4a87186fbea79c6ff28e8ecc1964104f61aac3d1
Tom McLaughlin <tom.mclaughlin@threatstack.com>
2016-12-26 17:01:17 -0500

    Move to maps for VPC outputs.

--- outputs.global.tf
+++ outputs.global.tf
@@ -31,140 +31,120 @@ output "aws_s3_terraform_state.bucket_id" {
 }
 
 
-output "vpc_public.route_table_ids_private" {
-  value = "${module.vpc_public.route_table_ids_private}"
+# VPCs
+output "vpc.vpc_id" {
+  value = {
+    private = "${module.vpc_private.vpc_id}",
+    public  = "${module.vpc_public.vpc_id}"
+  }
 }
 
-output "vpc_public.route_table_ids_public" {
-  value = "${module.vpc_public.route_table_ids_public}"
+output "vpc.vpc_names" {
+  value = [
+    "${module.vpc_private.vpc_name}",
+    "${module.vpc_public.vpc_name}"
+  ]
 }
 
-
-output "vpc_private.route_table_id_private" {
-  value = "${module.vpc_private.route_table_ids_private}"
+output "vpc.vpc_cidr_block" {
+  value = {
+    private = "${module.vpc_private.vpc_cidr_block}"
+    public  = "${module.vpc_public.vpc_cidr_block}"
+  }
 }
 
-output "vpc_private.route_table_id_public" {
-  value = "${module.vpc_private.route_table_ids_public}"
+output "vpc.default_security_group_id" {
+  value = {
+    private = "${module.vpc_private.vpc_default_security_group_id}"
+    public  = "${module.vpc_public.vpc_default_security_group_id}"
+  }
 }
 
-
-# vpc_private
-output "vpc_private.vpc_id" {
-  value = "${module.vpc_private.vpc_id}"
+output "vpc.route_table_ids_private" {
+  value = {
+    private = "${module.vpc_private.route_table_ids_private}"
+    public  = "${module.vpc_public.route_table_ids_private}"
+  }
 }
 
-output "vpc_private.vpc_name" {
-  value = "${module.vpc_private.vpc_name}"
+output "vpc.route_table_ids_public" {
+  value = {
+    private = "${module.vpc_private.route_table_ids_public}"
+    public  = "${module.vpc_public.route_table_ids_public}"
+  }
 }
 
-output "vpc_private.vpc_cidr_block" {
-  value = "${module.vpc_private.vpc_cidr_block}"
+output "vpc.subnet_availability_zones_private" {
+  value = {
+    private = "${module.vpc_private.subnet_availability_zones_private}"
+    public  = "${module.vpc_public.subnet_availability_zones_private}"
+  }
 }
 
-output "vpc_private.default_security_group_id" {
-  value = "${module.vpc_private.vpc_default_security_group_id}"
+output "vpc.subnet_cidr_blocks_private" {
+  value = {
+    private = "${module.vpc_private.subnet_cidr_blocks_private}"
+    public  = "${module.vpc_public.subnet_cidr_blocks_private}"
+  }
 }
 
-output "vpc_private.subnet_availability_zones_private" {
-  value ="${module.vpc_private.subnet_availability_zones_private}"
+output "vpc.subnet_ids_private" {
+  value = {
+    private = "${module.vpc_private.subnet_ids_private}"
+    public  = "${module.vpc_public.subnet_ids_private}"
+  }
 }
 
-output "vpc_private.subnet_cidr_blocks_private" {
-  value = ["${module.vpc_private.subnet_cidr_blocks_private}"]
+output "vpc.subnet_id_by_availability_zone_private" {
+  value = {
+    private = "${module.vpc_private.subnet_id_by_availability_zone_private}"
+    public  = "${module.vpc_public.subnet_id_by_availability_zone_private}"
+  }
 }
 
-output "vpc_private.subnet_ids_private" {
-  value = ["${module.vpc_private.subnet_ids_private}"]
+output "vpc.subnet_cidr_block_by_availability_zone_private" {
+  value = {
+    private = "${module.vpc_private.subnet_cidr_block_by_availability_zone_private}"
+    public  = "${module.vpc_public.subnet_cidr_block_by_availability_zone_private}"
+  }
 }
 
-output "vpc_private.subnet_id_by_availability_zone_private" {
-  value = "${module.vpc_private.subnet_id_by_availability_zone_private}"
+output "vpc.subnet_availability_zones_public" {
+  value = {
+    private = "${module.vpc_private.subnet_availability_zones_public}"
+    public  = "${module.vpc_public.subnet_availability_zones_public}"
+  }
 }
 
-output "vpc_private.subnet_cidr_block_by_availability_zone_private" {
-  value = "${module.vpc_private.subnet_cidr_block_by_availability_zone_private}"
+output "vpc.subnet_cidr_blocks_public" {
+  value = {
+    private = "${module.vpc_private.subnet_cidr_blocks_public}"
+    public  = "${module.vpc_public.subnet_cidr_blocks_public}"
+  }
 }
 
-output "vpc_private.subnet_availability_zones_public" {
-  value ="${module.vpc_private.subnet_availability_zones_public}"
+output "vpc.subnet_ids_public" {
+  value = {
+    private = "${module.vpc_private.subnet_ids_public}"
+    public  = "${module.vpc_public.subnet_ids_public}"
+  }
 }
 
-output "vpc_private.subnet_cidr_blocks_public" {
-  value = ["${module.vpc_private.subnet_cidr_blocks_public}"]
+output "vpc.subnet_id_by_availability_zone_public" {
+  value = {
+    private = "${module.vpc_private.subnet_id_by_availability_zone_public}"
+    public  = "${module.vpc_public.subnet_id_by_availability_zone_public}"
+  }
 }
 
-output "vpc_private.subnet_ids_public" {
-  value = ["${module.vpc_private.subnet_ids_public}"]
+output "vpc.subnet_cidr_block_by_availability_zone_public" {
+  value = {
+    private = "${module.vpc_private.subnet_cidr_block_by_availability_zone_public}"
+    public  = "${module.vpc_public.subnet_cidr_block_by_availability_zone_public}"
+  }
 }
 
-output "vpc_private.subnet_id_by_availability_zone_public" {
-  value = "${module.vpc_private.subnet_id_by_availability_zone_public}"
-}
-
-output "vpc_private.subnet_cidr_block_by_availability_zone_public" {
-  value = "${module.vpc_private.subnet_cidr_block_by_availability_zone_public}"
-}
-
-
-# vpc_public
-output "vpc_public.vpc_id" {
-  value = "${module.vpc_public.vpc_id}"
-}
-
-output "vpc_public.vpc_name" {
-  value = "${module.vpc_public.vpc_name}"
-}
-
-output "vpc_public.vpc_cidr_block" {
-  value = "${module.vpc_public.vpc_cidr_block}"
-}
-
-output "vpc_public.default_security_group_id" {
-  value = "${module.vpc_public.vpc_default_security_group_id}"
-}
-
-output "vpc_public.subnet_availability_zones_private" {
-  value ="${module.vpc_public.subnet_availability_zones_private}"
-}
-
-output "vpc_public.subnet_cidr_blocks_private" {
-  value = ["${module.vpc_public.subnet_cidr_blocks_private}"]
-}
-
-output "vpc_public.subnet_ids_private" {
-  value = ["${module.vpc_public.subnet_ids_private}"]
-}
-
-output "vpc_public.subnet_id_by_availability_zone_private" {
-  value = "${module.vpc_public.subnet_id_by_availability_zone_private}"
-}
-
-output "vpc_public.subnet_cidr_block_by_availability_zone_private" {
-  value = "${module.vpc_public.subnet_cidr_block_by_availability_zone_private}"
-}
-
-output "vpc_public.subnet_availability_zones_public" {
-  value ="${module.vpc_public.subnet_availability_zones_public}"
-}
-
-output "vpc_public.subnet_cidr_blocks_public" {
-  value = ["${module.vpc_public.subnet_cidr_blocks_public}"]
-}
-
-output "vpc_public.subnet_ids_public" {
-  value = ["${module.vpc_public.subnet_ids_public}"]
-}
-
-output "vpc_public.subnet_id_by_availability_zone_public" {
-  value = "${module.vpc_public.subnet_id_by_availability_zone_public}"
-}
-
-output "vpc_public.subnet_cidr_block_by_availability_zone_public" {
-  value = "${module.vpc_public.subnet_cidr_block_by_availability_zone_public}"
-}
-
-
+# VPC peering
 output "vpc_peer_public_private.vpc_peering_connection_id" {
   value = "${module.vpc_peer_public_private.vpc_peering_connection_id}"
 }
