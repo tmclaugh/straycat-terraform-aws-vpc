ab23a569536ff99b78ec9f23ccb56ee3d9598770
Tom McLaughlin <tom.mclaughlin@threatstack.com>
2017-02-16 14:02:42 -0500

    Add maps for gettig values by subnet type

--- outputs.tf
+++ outputs.tf
@@ -23,6 +23,49 @@ output "vpc.route_table_ids_public" {
   value = "${module.aws_vpc.route_table_ids_public}"
 }
 
+
+output "vpc.route_table_ids" {
+  value = {
+    private = "${module.aws_vpc.route_table_ids_private}"
+    public = "${module.aws_vpc.route_table_ids_public}"
+  }
+}
+
+output "vpc.subnet_availability_zones" {
+  value = {
+    private = "${module.aws_vpc.subnet_availability_zones_private}"
+    public = "${module.aws_vpc.subnet_availability_zones_public}"
+  }
+}
+
+output "vpc.subnet_cidr_blocks" {
+  value = {
+    private = "${module.aws_vpc.subnet_cidr_blocks_private}"
+    public = "${module.aws_vpc.subnet_cidr_blocks_public}"
+  }
+}
+
+output "vpc.subnet_ids" {
+  value = {
+    private = "${module.aws_vpc.subnet_ids_private}"
+    public = "${module.aws_vpc.subnet_ids_public}"
+  }
+}
+
+output "vpc.subnet_id_by_availability_zone" {
+  value = {
+    private = "${module.aws_vpc.subnet_id_by_availability_zone_private}"
+    public = "${module.aws_vpc.subnet_id_by_availability_zone_public}"
+  }
+}
+
+output "vpc.subnet_cidr_block_by_availability_zone" {
+  value = {
+    private = "${module.aws_vpc.subnet_cidr_block_by_availability_zone_private}"
+    public = "${module.aws_vpc.subnet_cidr_block_by_availability_zone_public}"
+  }
+}
+
 output "vpc.subnet_availability_zones_private" {
   value = "${module.aws_vpc.subnet_availability_zones_private}"
 }
@@ -43,16 +86,16 @@ output "vpc.subnet_cidr_block_by_availability_zone_private" {
   value = "${module.aws_vpc.subnet_cidr_block_by_availability_zone_private}"
 }
 
+output "vpc.route_table_ids_public" {
+  value = "${module.aws_vpc.route_table_ids_public}"
+}
+
 output "vpc.subnet_availability_zones_public" {
   value = "${module.aws_vpc.subnet_availability_zones_public}"
 }
 
 output "vpc.subnet_cidr_blocks_public" {
   value = "${module.aws_vpc.subnet_cidr_blocks_public}"
-}
-
-output "vpc.subnet_ids_public" {
-  value = "${module.aws_vpc.subnet_ids_public}"
 }
 
 output "vpc.subnet_id_by_availability_zone_public" {
